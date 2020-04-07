374c6bd37d0381a8c80d94826de3a60e7efbbf6f
Bartosz Malecki <Bartosz Malecki bartosz.piotr.malecki@cern.ch>
2020-04-07 18:25:28 +0200

    TimeInfo: provide readout time info based on measured values.

---
+++ common/scripts/SIMCOND/create_timeInfo.py
@@ -0,0 +1,169 @@
+#! /usr/bin/env python
+
+import csv
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from numpy import arange
+
+# config
+inputSignals = [0.1, 0.18, 0.32, 0.56, 1.0, 1.8, 3.2, 5.6, 10.0]
+thresholdValues = [x for x in arange(0.100, 0.500, 0.025)]
+
+#thresholdValues = [0.090, 0.210, 0.450, 0.930] # for xcheck with original width/delay plot
+
+
+def delayForSingleInputSignal(timeInfo, inputSignal):
+    record = timeInfo[inputSignal]
+    x = [el[0] for el in record]
+    y = [el[1] for el in record]
+
+    return [x, y]
+
+
+def widthForSingleInputSignal(timeInfo, inputSignal):
+    record = timeInfo[inputSignal]
+    x = [el[0] for el in record]
+    y = [el[2] for el in record]
+
+    return [x, y]
+
+
+# define data structures
+timeInfoDict = {}
+funcsDelay = {}
+funcsWidth = {}
+for inputSignal in inputSignals:
+    timeInfoDict[inputSignal] = []
+
+# read input from file
+with open('input/timeInfo/widthAndDelay.csv', mode='r') as csv_file:
+
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are: ' + "\t".join(row))
+            line_count += 1
+
+        timeInfoDict[float(row["inputSignal"])].append([
+            float(row["threshold"]),
+            float(row["delay"]),
+            float(row["width"])
+        ])
+        line_count += 1
+
+    print('Processed nr of lines: {}'.format(line_count))
+
+# create plots
+figDelay, axDelay = plt.subplots(1)
+figWidth, axWidth = plt.subplots(1)
+
+for inputSignal in inputSignals:
+
+    delay = delayForSingleInputSignal(timeInfoDict, inputSignal)
+    width = widthForSingleInputSignal(timeInfoDict, inputSignal)
+
+    if len(delay[0]) > 1 and len(width[0]) > 1:
+
+        funcDelay = interp1d(
+            delay[0], delay[1], kind='linear', bounds_error=False)
+        funcWidth = interp1d(
+            width[0], width[1], kind='linear', bounds_error=False)
+
+        funcsDelay[inputSignal] = funcDelay
+        funcsWidth[inputSignal] = funcWidth
+
+        axDelay.plot(
+            delay[0],
+            delay[1],
+            'o',
+            thresholdValues,
+            funcDelay(thresholdValues),
+            '--',
+            label=str(inputSignal))
+
+        axWidth.plot(
+            width[0],
+            width[1],
+            'o',
+            thresholdValues,
+            funcWidth(thresholdValues),
+            '--',
+            label=str(inputSignal))
+
+    else:
+        axDelay.plot(delay[0], delay[1], 'o', label=str(inputSignal))
+        axWidth.plot(width[0], width[1], 'o', label=str(inputSignal))
+
+# save plots
+plt.legend(loc='best')
+figDelay.savefig('output/timeInfo/delayVSthreshold.png')
+figWidth.savefig('output/timeInfo/widthVSthreshold.png')
+
+# create time info in proper format
+outputFileWidth = open('output/timeInfo/width.dat', 'w')
+outputFileDelay = open('output/timeInfo/delay.dat', 'w')
+
+figDelayInThreshold, axDelayInThreshold = plt.subplots(1)
+figWidthInThreshold, axWidthInThreshold = plt.subplots(1)
+
+for threshold in thresholdValues:
+    delayInThreshold = []
+    widthInThreshold = []
+    for inputSignal in inputSignals:
+        if inputSignal in funcsDelay:
+            delayInThreshold.append([
+                float(inputSignal),
+                float(funcsDelay[inputSignal](threshold))
+            ])
+        else:
+            pass
+            #delay = delayForSingleInputSignal(timeInfoDict, inputSignal)
+            #delayInThreshold.append([float(inputSignal), float(delay[1][0])])
+
+        if inputSignal in funcsWidth:
+            widthInThreshold.append([
+                float(inputSignal),
+                float(funcsWidth[inputSignal](threshold))
+            ])
+        else:
+            pass
+            #width = widthForSingleInputSignal(timeInfoDict, inputSignal)
+            #widthInThreshold.append([float(inputSignal), float(width[1][0])])
+
+    # final plots
+    axDelayInThreshold.plot([el[0] for el in delayInThreshold],
+                            [el[1] for el in delayInThreshold],
+                            'o-',
+                            label=str(threshold))
+    axDelayInThreshold.set_xscale('log')
+    axDelayInThreshold.set_ylim([0, 45])
+    plt.grid(True)
+
+    axWidthInThreshold.plot([el[0] for el in widthInThreshold],
+                            [el[1] for el in widthInThreshold],
+                            'o-',
+                            label=str(threshold))
+    axWidthInThreshold.set_xscale('log')
+    axWidthInThreshold.set_ylim([0, 45])
+    plt.grid(True)
+
+    # print to file
+    outputFileDelay.write("% threshold = {:.3f}\n".format(threshold))
+    for el in delayInThreshold:
+        outputFileDelay.write("{:.2f}\t{:.3f}\n".format(el[0], el[1]))
+    outputFileDelay.write("\n")
+
+    outputFileWidth.write("% threshold = {:.3f}\n".format(threshold))
+    for el in widthInThreshold:
+        outputFileWidth.write("{:.2f}\t{:.3f}\n".format(el[0], el[1]))
+    outputFileWidth.write("\n")
+
+# save plots
+plt.legend(loc='best')
+figDelayInThreshold.savefig('output/timeInfo/delayVSinputSignal.png')
+figWidthInThreshold.savefig('output/timeInfo/widthVSinputSignal.png')
+
+# cleanup
+outputFileDelay.close()
+outputFileWidth.close()
